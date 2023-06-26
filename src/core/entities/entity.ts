f0d6ddaf24baf4e668cc4b9229491fe5499bc84d
azanniel <leo.azannielttt@gmail.com>
2023-06-26 10:11:56 -0400

    ✨ add getters and setters

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,16 @@
+import { UniqueEntityId } from "./unique-entity-id"
+
+export class Entity<Props> {
+  private _id: UniqueEntityId
+  protected props: Props
+
+  protected constructor(props: Props, id?: UniqueEntityId) {
+    this._id = id ?? new UniqueEntityId()
+    this.props = props
+  }
+
+  get id() {
+    return this._id
+  }
+
+}
