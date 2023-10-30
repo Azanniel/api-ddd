5bfe9e4eea777f1c5878746729e7fc411b00da17
azanniel <leo.azannielttt@gmail.com>
2023-10-30 16:16:29 -0400

    ✅ add tests to auth and register student

---
+++ src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,78 @@
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { AuthenticateStudentUseCase } from './authenticate-student'
+import { makeStudent } from 'test/factories/make-student'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+let sut: AuthenticateStudentUseCase /** SUT -> System under test */
+
+describe('Authenticate student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    sut = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate a student', async () => {
+    const student = makeStudent({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.create(student)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const student = makeStudent({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.create(student)
+
+    const result = await sut.execute({
+      email: 'wrong-email',
+      password: '123456',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(WrongCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const student = makeStudent({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.create(student)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: 'wrong-password',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(WrongCredentialsError)
+  })
+})
