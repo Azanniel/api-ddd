ad4cff8fada63db599eb23f30e536f2625a2fbb3
azanniel <leo.azannielttt@gmail.com>
2023-11-29 22:26:02 -0400

    ✨ edit answer

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -7,6 +7,7 @@ import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerAttachmentsRepository } from '../repositories/answer-attachments-repository'
 import { NotAllowedError } from '@/core/errors/not-allowed-error'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
+import { Injectable } from '@nestjs/common'
 
 interface EditAnswerUseCaseRequest {
   authorId: string
@@ -22,6 +23,7 @@ type EditAnswerUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class EditAnswerUseCase {
   constructor(
     private answersRepository: AnswersRepository,
