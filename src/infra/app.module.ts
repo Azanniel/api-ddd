9f98082540b0b12a4a1290ed2c9c836d5ce397e5
azanniel <leo.azannielttt@gmail.com>
2024-01-12 00:42:11 -0400

    ✨ create event module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { EnvModule } from './env/env.module'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
+import { EventsModule } from './events/events.module'
 
 @Module({
-  imports: [EnvModule, AuthModule, HttpModule],
+  imports: [EnvModule, AuthModule, HttpModule, EventsModule],
 })
 export class AppModule {}
